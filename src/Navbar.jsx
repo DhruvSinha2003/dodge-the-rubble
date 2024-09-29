5f4726b2f33e2a2a4e720367983cae4fa46ca363
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-09-29 22:04:32 +0530

    theme toggle

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  function toggleTheme() {
+    setTheme((currTheme) => (currTheme === "light" ? "dark" : "light"));
+  }
   return (
     <div className="navbar">
       <div className="logo"></div>
       <div className="hamburger"></div>
+      <div className="theme">
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </div>
     </div>
   );
 };
